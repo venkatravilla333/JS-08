b2717442826620200fbe86fcc5cbef7478a1047f
konda ravilla <kondaravilla000@gmail.com>
2024-07-02 12:30:46 +0530

    loops

---
+++ Loops/index.js
@@ -0,0 +1,50 @@
+
+
+//LOOPS
+
+var x = 'kohli'
+
+//without using loop
+
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+
+//with using loop
+
+// for loop
+
+// for (var i = 1; i <= 10; i++){
+//   console.log(i, x)
+// }
+
+//while loop
+
+// var i = 1
+
+// while (i<=10) {
+//   console.log(i, x)
+//   i++
+// }
+
+//do while loop
+
+// var i = 11
+
+// do {
+//   console.log(i, x)
+//   i++
+// }while(i<=10)
+
+//Infinity loop
+
+// for (var i = 0; i >= 0; i++){
+//   console.log(i, x)
+// }
