3ef57da93cb199347cb37bebd431bc978952060b
konda ravilla <kondaravilla000@gmail.com>
2024-07-03 12:33:25 +0530

    loops completed

--- Loops/index.js
+++ Loops/index.js
@@ -48,3 +48,70 @@ var x = 'kohli'
 // for (var i = 0; i >= 0; i++){
 //   console.log(i, x)
 // }
+
+//break
+
+// for (var i = 1; i <= 100; i++){
+//   if(i===10) break
+//   console.log(i, x)
+// }
+
+
+//for in loop and for of loop
+
+
+//for in loop
+// var noniterabelds = {
+//   name: 'kohli',
+//   age: 35,
+//   state: 'dehli',
+//   country: 'indai',
+//   color: 'red',
+//   height: 5,
+  
+// }
+
+// console.log(noniterabelds)
+//without loop
+
+// console.log(noniterabelds.name)
+// console.log(noniterabelds.age)
+// console.log(noniterabelds.state)
+// console.log(noniterabelds.country)
+// console.log(noniterabelds.color)
+// console.log(noniterabelds.height)
+
+//with loop
+
+// for (var key in obj) {
+//   console.log(key, obj[key])
+// }
+
+//for of loop
+
+var iterableds = [1, 2, 3, 4, 5]
+
+// console.log(arr)
+
+// without loop
+
+// console.log(arr[0])
+// console.log(arr[1])
+// console.log(arr[2])
+// console.log(arr[3])
+// console.log(arr[4])
+
+// var iteratorObj = iterableds[Symbol.iterator]()
+// console.log(iteratorObj.next())
+// console.log(iteratorObj.next())
+// console.log(iteratorObj.next())
+// console.log(iteratorObj.next())
+// console.log(iteratorObj.next())
+// console.log(iteratorObj.next())
+
+// for (var value of iterableds) {
+//   console.log(value)
+// }
+
+
+
